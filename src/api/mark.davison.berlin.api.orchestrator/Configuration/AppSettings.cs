95bba5a220169238e0206739fbe51297fb2d36b3
mark.davison <markdavison0+github@gmail.com>
2024-05-14 09:12:16 +1200

    fix update stories cron expression

--- src/api/mark.davison.berlin.api.orchestrator/Configuration/AppSettings.cs
+++ src/api/mark.davison.berlin.api.orchestrator/Configuration/AppSettings.cs
@@ -9,7 +9,7 @@ public class AppSettings : IAppSettings
     public int STORIES_PER_CRON_UPDATE { get; set; } = 20;
     public string JOB_CHECK_RATE { get; set; } = "*/15 * * * *";
     public string MONTHLY_STORY_NOTIFICATIONS_RATE { get; set; } = "0 8 1,15 * *";
-    public string STORY_UPDATE_RATE { get; set; } = "0 3 * * *";
+    public string STORY_UPDATE_RATE { get; set; } = "0 */3 * * *";
     public string CRON_TIMEZONE { get; set; } = "LOCAL";
     public bool PRODUCTION_MODE { get; set; }
 }
