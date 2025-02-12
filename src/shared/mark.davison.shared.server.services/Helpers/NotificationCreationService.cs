22d3150e51906ba491323370e67d5dc56cec8d4d
mark.davison <markdavison0+github@gmail.com>
2025-02-12 15:06:55 +1300

    put title at start of update text

--- src/shared/mark.davison.shared.server.services/Helpers/NotificationCreationService.cs
+++ src/shared/mark.davison.shared.server.services/Helpers/NotificationCreationService.cs
@@ -9,12 +9,12 @@ public sealed class NotificationCreationService : INotificationCreationService
         var updateChapterNumbers = updates.Select(_ => _.CurrentChapters).OrderByDescending(_ => _).ToList();
         var firstUpdate = updates.SelectMany(_ => _.ChapterInfo.Values).OrderByDescending(_ => _.Number).LastOrDefault();
 
-        builder.AppendLine("= STORY UPDATED =");
+        builder.AppendLine($"= {story.Name} UPDATED =");
         builder.AppendLine("|");
         builder.AppendLine($"|  Name: {story.Name}");
         builder.AppendLine($"|  Chapters: {string.Join(", ", updateChapterNumbers)}");
         builder.AppendLine($"|  Status: {(story.Complete ? "Complete" : "In progress")}");
-        builder.AppendLine($"|  Site: {site.LongName}({site.ShortName})");
+        builder.AppendLine($"|  Site: {site.LongName} ({site.ShortName})");
         builder.AppendLine($"|  Url: {firstUpdate?.Address ?? story.Address}");
         builder.AppendLine("|");
         builder.AppendLine("===================");
@@ -28,12 +28,12 @@ public sealed class NotificationCreationService : INotificationCreationService
 
         var chapterInfo = info.ChapterInfo.Values.FirstOrDefault(_ => _.Number == info.CurrentChapters);
 
-        builder.AppendLine("= STORY UPDATED =");
+        builder.AppendLine($"= {info.Name} UPDATED =");
         builder.AppendLine("|");
         builder.AppendLine($"|  Name: {info.Name}{(info.Name == story.Name ? string.Empty : $" - previously {story.Name}")}");
         builder.AppendLine($"|  Chapters: {info.CurrentChapters}/{(info.TotalChapters?.ToString() ?? "?")} - previously {story.CurrentChapters}/{(story.TotalChapters?.ToString() ?? "?")}");
         builder.AppendLine($"|  Status: {(info.IsCompleted ? "Complete" : "In progress")} - previously {(story.Complete ? "Complete" : "In progress")}");
-        builder.AppendLine($"|  Site: {site.LongName}({site.ShortName})");
+        builder.AppendLine($"|  Site: {site.LongName} ({site.ShortName})");
         builder.AppendLine($"|  Url: {chapterInfo?.Address ?? story.Address}");
         builder.AppendLine("|");
         builder.AppendLine("===================");
