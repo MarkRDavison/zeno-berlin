f2ebb673e58e4dd473fb6ddc222012913bf6eba4
mark.davison <markdavison0+github@gmail.com>
2025-12-30 15:24:57 +1300

    Add migrations

---
+++ src/api/migrations/mark.davison.berlin.api.migrations.sqlite/Migrations/20240427050730_AlphaModels.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mark.davison.berlin.api.migrations.sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlphaModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Sub = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false),
+                    First = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Last = table.Column<string>(type: "TEXT", maxLength: 62554, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Admin = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fandoms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IsHidden = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsUserSpecified = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ExternalName = table.Column<string>(type: "TEXT", nullable: false),
+                    ParentFandomId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fandoms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fandoms_Fandoms_ParentFandomId",
+                        column: x => x.ParentFandomId,
+                        principalTable: "Fandoms",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Fandoms_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sites",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ShortName = table.Column<string>(type: "TEXT", nullable: false),
+                    LongName = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sites_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UpdateTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UpdateTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UpdateTypes_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    ExternalId = table.Column<string>(type: "TEXT", nullable: false),
+                    CurrentChapters = table.Column<int>(type: "INTEGER", nullable: false),
+                    TotalChapters = table.Column<int>(type: "INTEGER", nullable: true),
+                    Complete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Favourite = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LastChecked = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastAuthored = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    SiteId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UpdateTypeId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stories_Sites_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Sites",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Stories_UpdateTypes_UpdateTypeId",
+                        column: x => x.UpdateTypeId,
+                        principalTable: "UpdateTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Stories_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryFandomLinks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    StoryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FandomId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryFandomLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StoryFandomLinks_Fandoms_FandomId",
+                        column: x => x.FandomId,
+                        principalTable: "Fandoms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoryFandomLinks_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoryFandomLinks_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryUpdates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CurrentChapters = table.Column<int>(type: "INTEGER", nullable: false),
+                    TotalChapters = table.Column<int>(type: "INTEGER", nullable: true),
+                    Complete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LastAuthored = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    StoryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryUpdates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StoryUpdates_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoryUpdates_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fandoms_ParentFandomId",
+                table: "Fandoms",
+                column: "ParentFandomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fandoms_UserId",
+                table: "Fandoms",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sites_UserId",
+                table: "Sites",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stories_SiteId",
+                table: "Stories",
+                column: "SiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stories_UpdateTypeId",
+                table: "Stories",
+                column: "UpdateTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stories_UserId",
+                table: "Stories",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryFandomLinks_FandomId",
+                table: "StoryFandomLinks",
+                column: "FandomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryFandomLinks_StoryId",
+                table: "StoryFandomLinks",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryFandomLinks_UserId",
+                table: "StoryFandomLinks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryUpdates_StoryId",
+                table: "StoryUpdates",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryUpdates_UserId",
+                table: "StoryUpdates",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UpdateTypes_UserId",
+                table: "UpdateTypes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StoryFandomLinks");
+
+            migrationBuilder.DropTable(
+                name: "StoryUpdates");
+
+            migrationBuilder.DropTable(
+                name: "Fandoms");
+
+            migrationBuilder.DropTable(
+                name: "Stories");
+
+            migrationBuilder.DropTable(
+                name: "Sites");
+
+            migrationBuilder.DropTable(
+                name: "UpdateTypes");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ src/api/migrations/mark.davison.berlin.api.migrations.sqlite/Migrations/20240428033649_AddAuthor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mark.davison.berlin.api.migrations.sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IsUserSpecified = table.Column<bool>(type: "INTEGER", nullable: false),
+                    SiteId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    ParentAuthorId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Authors_Authors_ParentAuthorId",
+                        column: x => x.ParentAuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Authors_Sites_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Sites",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Authors_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryAuthorLinks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    StoryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryAuthorLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StoryAuthorLinks_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoryAuthorLinks_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoryAuthorLinks_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_ParentAuthorId",
+                table: "Authors",
+                column: "ParentAuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_SiteId",
+                table: "Authors",
+                column: "SiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_UserId",
+                table: "Authors",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryAuthorLinks_AuthorId",
+                table: "StoryAuthorLinks",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryAuthorLinks_StoryId",
+                table: "StoryAuthorLinks",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryAuthorLinks_UserId",
+                table: "StoryAuthorLinks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StoryAuthorLinks");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
